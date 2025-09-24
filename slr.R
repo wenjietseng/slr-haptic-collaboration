844c9e0f6799be5c411a3118508cd31f2ffa4ac4
Wen-Jie Tseng <wenjietseng@gmail.com>
2025-09-24 11:16:02 +0200

    deciding keywords

--- slr.R
+++ slr.R
@@ -1,5 +1,5 @@
 ## packages
-packages <- c("easyPubMed", "dplyr"
+packages <- c("easyPubMed", "dplyr",
               "litsearchr", "stopwords", "igraph",
               "ggplot2", "ggraph", "ggrepel",
               "rvest", "polite", "remotes", "httr2"
@@ -73,15 +73,11 @@ session |> scrape()
 # one ris is based on the final list
 # another is based on the naive search on ACM DL,
 # collaborative virtual environment AND haptic AND virtual reality
-setwd("./Documents/slr-haptic-collaboration/")
 search_directory <- "~/Documents/slr-haptic-collaboration/naive_search_0923/"
 naive_import <- litsearchr::import_results(search_directory, verbose = TRUE)
 naive_results <- litsearchr::remove_duplicates(naive_import, field = "title", method = "string_osa")
 table(naive_import$filename)
 table(naive_results$filename)
-# final_list <- subset(naive_import, naive_import$filename == "/Users/tseng/Documents/slr-haptic-collaboration/naive_search/naive-final-list.ris")
-# final_list$issn
-# str(final_list)
 
 ## let's keep conference and journals
 ggplot(naive_results, aes(x=source_type)) +
@@ -94,36 +90,35 @@ dim(naive_results)
 dim(dta)
 
 is.na(dta$year)
-ifelse(is.na(dta$year), dta$date_generated, substr(dta$year, start = 1, stop = 4))
+dta$cleaned_year <- ifelse(is.na(dta$year), substr(dta$date_generated, start = 1, stop = 4), dta$year)
 
+ggplot(dta, aes(x=cleaned_year)) +
+  geom_bar() +
+  theme(axis.text.x = element_text(angle = -60))
 
-dta |> filter(is.na(year))
-ggplot(dta, aes(x=year)) +
-  geom_bar()
-  
-
+## extract keywords from titles and abstracts using the fakerake algorithm.
 rakedkeywords <-
   litsearchr::extract_terms(
-    text = paste(naive_results$title, naive_results$abstract),
+    text = paste(dta$title, dta$abstract),
     method = "fakerake",
-    min_freq = 4,
+    min_freq = 5, # if min_freq equals (3,4,5), the algorithm extracts (686,355,226) keywords.
     ngrams = TRUE,
     min_n = 2,
     language = "English"
   )
 rakedkeywords
 
-lapply(final_list$issn, litsearchr::clean_keywords) |>
-  lapply(function(x) {strsplit(x, "[,;]")}) |>
-  unlist() |>
-  tolower() |> table()
+## unused code
+# lapply(final_list$issn, litsearchr::clean_keywords) |>
+#   lapply(function(x) {strsplit(x, "[,;]")}) |>
+#   unlist() |>
+#   tolower() |> table()
 
-# I go for keywords...
 taggedkeywords <-
   litsearchr::extract_terms(
-    keywords = naive_results$issn,
+    keywords = dta$keywords,
     method = "tagged",
-    min_freq = 2,
+    min_freq = 4, # if min_freq equals (2,3,4), the algorithm extracts (212,85,54) keywords.
     ngrams = TRUE,
     min_n = 2,
     language = "English"
@@ -131,11 +126,25 @@ taggedkeywords <-
 taggedkeywords
 
 all_keywords <- unique(append(taggedkeywords, rakedkeywords))
+all_keywords # 251
 
-# co‐occurrence  network
+### co‐occurrence  network
+# an example to show how this work
+dfm <- create_dfm(
+  elements = c(
+    "Cross-scale occupancy dynamics of a postfire specialist 
+    in response to variation across a fire regime", # data 1
+    "Variation in home-range size of Black-backed Woodpeckers", # data 2
+    "Black-backed woodpecker occupancy in burned and beetle-killed forests" # data 3
+  ),
+  features = c("occupancy", "variation", "black-backed woodpecker", "burn")
+)
+as.matrix(dfm)
+# which keywords co-occur in the same title or abstract, create a matrix[text, keywords]
+
 naivedfm <-
   litsearchr::create_dfm(
-    elements = paste(naive_results$title, naive_results$abstract), # keywords here?
+    elements = paste(dta$title, dta$abstract),
     features = all_keywords
   )
 
@@ -158,15 +167,53 @@ plot(sort(igraph::strength(naivegraph)),
      col = "steelblue"
      )
 
-cutoff <- 
-  find_cutoff(graph=naivegraph, method = "cumulative", 
+
+cutoff_medium <-
+  find_cutoff(graph=naivegraph, method = "cumulative",
               percent = 0.8,
               imp_method = "strength"
+  ) 
+cutoff_medium
+
+cutoff_core <-
+  find_cutoff(graph=naivegraph, method = "changepoint",
+              knot_num = 3,
+              imp_method = "strength"
   )
+cutoff_core
 
-reduced_graph <- reduce_graph(naivegraph, cutoff_strength = cutoff)
+plot(sort(igraph::strength(naivegraph)),
+     ylab = "Node strength",
+     xlab = "Rank",
+     main = "Ranked node strengths",
+     type = "l",
+     lwd = "3",
+     col = "steelblue"
+)
+abline(h = cutoff_core, col="red")
+abline(h = cutoff_medium, col="red", lty = 3, lwd = 2)
 
+reduced_graph_core <- reduce_graph(naivegraph, cutoff_strength = cutoff_core[1])
+plot(reduced_graph_core)
+search_terms_core <- get_keywords(reduced_graph_core)
+search_terms_core
+## compare to the naive search we used ("collaborative virtual environment" OR
+## "shared virtual environment" OR "remote collaboration") AND "haptic" 
+## the core keywords include VR, AR, MR, and XR.
 
-plot(reduced_graph)
-search_terms <- get_keywords(reduced_graph)
-write.csv(search_terms, "../search_termsc08.csv")
+## now we check the medium strength keywords.
+
+reduced_graph_medium <- reduce_graph(naivegraph, cutoff_strength = cutoff_medium)
+plot(reduced_graph_medium)
+search_terms_medium <- get_keywords(reduced_graph_medium)
+search_terms_medium
+
+idx <- sapply(search_terms_core, function(x) {
+  which(search_terms_medium == x)  
+})
+search_terms_medium <- search_terms_medium[-idx]
+
+c(search)
+write.table(as.data.frame(sort(search_terms)), "./search_terms/2409.csv",
+            row.names = FALSE, col.names = FALSE,
+            sep = ",")
